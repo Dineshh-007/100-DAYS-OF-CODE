2d4305095698b569c49c88c6d7b33f3daed6bf72
Dinesh E <dineshedine007@gmail.com>
2025-04-25 14:25:37 +0530

    Delete Day_2_tip_calculator_project.py

--- Day_2_tip_calculator_project.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator!")
-print(" ")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-amount= (bill+(bill*tip/100))/5
-
-print(f"Each person should pay: {round(amount,2)}")
