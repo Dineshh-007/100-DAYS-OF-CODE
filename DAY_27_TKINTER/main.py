260c098e4ea547f4258ee2e1409ab2ecfe74b709
Dineshh-007 <dineshedine007@gmail.com>
2025-06-16 19:30:53 +0530

    Updated main.py in Day 27 TKinter project

--- DAY_27_TKINTER/main.py
+++ DAY_27_TKINTER/main.py
@@ -1,28 +1,33 @@
 from tkinter import *
 
 window = Tk()
-window.minsize(500,500)
-
-my_label = Label(text="Hi...I'm Dinesh",font=("Aerial", 24 , "italic"))
-my_label.pack()
-
-my_label["text"] = "New text"
-my_label.config(text="New text")
+window.minsize(500,300)
+window.config(padx=20 ,pady=20)
 
 def button_clicked():
     my_label["text"] = "New text"
     new_text = input.get()
     my_label.config(text=new_text)
-    print("I got clicked...")
 
-#Buttons
+
+my_label = Label(text="Hi...I'm Dinesh",font=("Aerial", 24 , "italic"))
+
+my_label["text"] = "New text"
+my_label.config(text="New text")
+my_label.grid(column=0, row=0)
+
+#Button
 button = Button(text="Click me",command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+#New Button
+new_button = Button(text="Click me,bruh")
+new_button.grid(column=2, row=0)
 
 # #Entry
-
 input = Entry(width=10)
-input.pack()
+input.grid(column=3,row=3)
+
 
 
 
